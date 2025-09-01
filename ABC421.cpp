37c7c6b224d3dfa2802ea96d218ff75ae9c35f51
KomeijiArmia <106058352+Komeiji-Armia@users.noreply.github.com>
2025-09-01 15:13:13 +0900

    ABC421-C

---
+++ ABC421.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	string S;
+	cin >> S;
+
+	vector<int> B(N);
+
+	int j = 0;
+	for (int i = 0; i < 2 * N; i++) {
+
+		if (S[i] == 'B') {
+			B[j] = i;
+			j++;
+		}
+
+	}
+
+	long long ANS1=0, ANS2=0;
+
+	for (int i = 0; i < N; i++) {
+		ANS1 += abs(((2*i)+1)-B[i]);//ABパターン
+		ANS2 += abs(((2*i))-B[i]);//BAパターン
+
+		//cout << abs(((2 * i) + 1) - B[i]) << " " << abs(((2 * i)) - B[i]) << endl;
+	}
+
+	cout << min(ANS1,ANS2) << endl;
+
+	return 0;
+
+}
